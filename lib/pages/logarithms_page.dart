9099438729f800139c406123d8933ef3f6d69a38
gofer-k <khofman250377@gmail.com>
2025-09-03 14:50:52 +0200

    fix: final logarithms contents

--- lib/pages/logarithms_page.dart
+++ lib/pages/logarithms_page.dart
@@ -13,8 +13,26 @@ class LogarithmsPage extends StatefulWidget{
 class _LogarithmsPageState extends State<LogarithmsPage> {
   @override
   Widget build(BuildContext context) {
-    final leftColScale = 1.6;
-    final rightColScale = 1.3;
+    final leftColScale = 2.0;
+    final rightColScale = 2.0;
+    final horizontalPaddingItem = 4.0;
+    final verticalPaddingItem = 4.0;
+    final listItemDecoration = BoxDecoration(
+      color: Colors.grey.shade300, // Background color of the "card"
+      border: Border.all(
+        color: Colors.grey.shade400, // Color of the outline
+        width: 1.0,                // Thickness of the outline
+        ),
+      borderRadius: BorderRadius.circular(8.0), // Rounded corners for the card-like look
+      boxShadow: [ // Optional: Add shadow similar to a Card
+        BoxShadow(
+          color: Colors.grey.withAlpha(128),
+          spreadRadius: 2,
+          blurRadius: 5,
+          offset: Offset(0, 3), // changes position of shadow
+        ),
+      ],
+    );
 
     return BackgroundContainer(
       beginColor: Colors.grey.shade300,
@@ -29,94 +47,285 @@ class _LogarithmsPageState extends State<LogarithmsPage> {
               widget.title, style: Theme.of(context).textTheme.headlineMedium,
           ),
         ),
-        body: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 1.0, vertical: 2.0),
-            child: Table(
-              columnWidths: const <int, TableColumnWidth>{
-                0: FlexColumnWidth(0.7),
-                1: FlexColumnWidth(0.3)
-              },
-              children: [
-                tableRow(horizontalPadding: 2.0, verticalPadding: 1.0, cells: [
-                  DisplayExpression(context: context, expression: "x = \log_{a} {b} \\Leftrightarrow a_x = b", scale: leftColScale),
-                  DisplayExpression(context: context, expression: "a \\not\\equiv 1", scale: rightColScale),
-                ]),
-                tableRow(horizontalPadding: 2.0, verticalPadding: 1.0, cells: [
-                  DisplayExpression(context: context, expression: r'x = log_a a^x', scale: leftColScale),
-                  DisplayExpression(context: context, expression: 'x \\in R, a \\not\\equiv 1', scale: rightColScale),
-                ]),
-                tableRow(horizontalPadding: 2.0, verticalPadding: 1.0, cells: [
-                  DisplayExpression(context: context, expression: "b = a^{log_a b}", scale: leftColScale),
-                  DisplayExpression(context: context, expression: "b > 0, a \\not\\equiv 1", scale: rightColScale),
-                ]),
-                tableRow(horizontalPadding: 2.0, verticalPadding: 1.0, cells: [
-                  DisplayExpression(context: context, expression: "0 = log_a a^0 = log_a 1", scale: leftColScale),
-                  DisplayExpression(context: context, expression: "a \\not\\equiv 1", scale: rightColScale),
-                ]),
-                tableRow(horizontalPadding: 2.0, verticalPadding: 1.0, cells: [
-                  DisplayExpression(context: context, expression: "1 = log_a a^1 = log_a a", scale: leftColScale),
-                  DisplayExpression(context: context, expression: "a \\not\\equiv 1", scale: rightColScale),
-                ]),
-                tableRow(horizontalPadding: 2.0, verticalPadding: 1.0, cells: [
-                  // This formula: r'.... \log_a 'frac{1}{a}...' do only work !!!
-                  DisplayExpression(context: context, expression: r'-1 = log_a a^{-1} = log_a \frac{1}{a}', scale: leftColScale),
-                  DisplayExpression(context: context, expression: "a \\not\\equiv 1, a \\not\\equiv 0", scale: rightColScale),
-                ]),
-                tableRow(horizontalPadding: 2.0, verticalPadding: 1.0, cells: [
-                  DisplayExpression(context: context, expression: "b^y = a^{y\log_a b}", scale: leftColScale),
-                  DisplayExpression(context: context, expression: "a \\not\\equiv 1", scale: rightColScale),
-                ]),
-                tableRow(horizontalPadding: 2.0, verticalPadding: 1.0, cells: [
-                  DisplayExpression(context: context, expression: "a = b^{log_b a} = a^{log_b a * log_a b}", scale: leftColScale),
-                  DisplayExpression(context: context, expression: "a \\not\\equiv 1, b \\not\\equiv 1", scale: rightColScale),
-                ]),
-                tableRow(horizontalPadding: 2.0, verticalPadding: 1.0, cells: [
-                  DisplayExpression(context: context, expression: "1 = log_b a * log_a b", scale: leftColScale),
-                  DisplayExpression(context: context, expression: "a \\not\\equiv 1, b \\not\\equiv 1", scale: rightColScale),
-                ]),
-                tableRow(horizontalPadding: 2.0, verticalPadding: 1.0, cells: [
-                  DisplayExpression(context: context, expression: "log_a uv = log_a u + log_a v", scale: leftColScale),
-                  DisplayExpression(context: context, expression: "u > 0, v > 0", scale: rightColScale),
-                ]),
-                tableRow(horizontalPadding: 2.0, verticalPadding: 1.0, cells: [
-                  DisplayExpression(context: context, expression: "log_a uv = log_a u + log_a v", scale: leftColScale),
-                  DisplayExpression(context: context, expression: "v >0, v > 0", scale: rightColScale),
-                ]),
-                tableRow(horizontalPadding: 2.0, verticalPadding: 1.0, cells: [
-                  DisplayExpression(context: context, expression: "log_a {u^z} = z\\log_a u", scale: leftColScale),
-                  DisplayExpression(context: context, expression: "u > 0", scale: rightColScale),
-                ]),
-                tableRow(horizontalPadding: 2.0, verticalPadding: 1.0, cells: [
-                  // This formula: r'.... \log_a 'frac{1}{a}...' do only work !!!
-                  DisplayExpression(context: context, expression: r'log_a \frac{u}{v} = log_a u - log_a v', scale: leftColScale),
-                  DisplayExpression(context: context, expression: "a \\not\\equiv 1, v \\not\\equiv 0", scale: rightColScale),
-                ]),
-                tableRow(horizontalPadding: 2.0, verticalPadding: 1.0, cells: [
-                  DisplayExpression(context: context, expression: "log_a u^{-1} = -log_a u", scale: leftColScale),
-                  DisplayExpression(context: context, expression: "a \\not\\equiv 1", scale: rightColScale),
-                ]),
-              ]
-            ),
-          ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 2.0, vertical: 2.0),
+          child: ListView(
+            children: [
+              listItem(
+                horizontalPadding: horizontalPaddingItem,
+                verticalPadding: verticalPaddingItem,
+                content:
+                  DisplayExpression(
+                    context: context,
+                    decoration: listItemDecoration,
+                    expression: "x = log_{a} {b} \\Leftrightarrow a_x = b",
+                    scale: leftColScale),
+                dialogContent:
+                  DisplayExpression(
+                    context: context,
+                    decoration: listItemDecoration,
+                    expression: "a \\not\\equiv 1",
+                    scale: rightColScale),
+              ),
+              listItem(
+                horizontalPadding: horizontalPaddingItem,
+                verticalPadding: verticalPaddingItem,
+                content:
+                DisplayExpression(
+                    context: context,
+                    decoration: listItemDecoration,
+                    expression: r'x = log_a a^x',
+                    scale: leftColScale),
+                dialogContent:
+                DisplayExpression(
+                    context: context,
+                    decoration: listItemDecoration,
+                    expression: 'x \\in R, a \\not\\equiv 1',
+                    scale: rightColScale),
+              ),
+              listItem(
+                horizontalPadding: horizontalPaddingItem,
+                verticalPadding: verticalPaddingItem,
+                content:
+                DisplayExpression(
+                    context: context,
+                    decoration: listItemDecoration,
+                    expression: "b = a^{log_a b}",
+                    scale: leftColScale),
+                dialogContent:
+                DisplayExpression(
+                    context: context,
+                    decoration: listItemDecoration,
+                    expression: "b > 0, a \\not\\equiv 1",
+                    scale: rightColScale),
+              ),
+              listItem(
+                horizontalPadding: horizontalPaddingItem,
+                verticalPadding: verticalPaddingItem,
+                content:
+                DisplayExpression(
+                    context: context,
+                    decoration: listItemDecoration,
+                    expression: "0 = log_a a^0 = log_a 1",
+                    scale: leftColScale),
+                dialogContent:
+                DisplayExpression(
+                    context: context,
+                    decoration: listItemDecoration,
+                    expression: "a \\not\\equiv 1",
+                    scale: rightColScale),
+              ),
+              listItem(
+                horizontalPadding: horizontalPaddingItem,
+                verticalPadding: verticalPaddingItem,
+                content:
+                DisplayExpression(
+                    context: context,
+                    decoration: listItemDecoration,
+                    expression: "1 = log_a a^1 = log_a a",
+                    scale: leftColScale),
+                dialogContent:
+                DisplayExpression(
+                    context: context,
+                    decoration: listItemDecoration,
+                    expression: "a \\not\\equiv 1",
+                    scale: rightColScale),
+              ),
+              listItem(
+                horizontalPadding: horizontalPaddingItem,
+                verticalPadding: verticalPaddingItem,
+                content:
+                DisplayExpression(
+                    context: context,
+                    decoration: listItemDecoration,
+                    // This formula: r'.... \log_a 'frac{1}{a}...' do only work !!!
+                    expression: r'-1 = log_a a^{-1} = log_a \frac{1}{a}',
+                    scale: leftColScale),
+                dialogContent:
+                DisplayExpression(
+                    context: context,
+                    decoration: listItemDecoration,
+                    expression: "a \\not\\equiv 1, a \\not\\equiv 0",
+                    scale: rightColScale),
+              ),
+              listItem(
+                horizontalPadding: horizontalPaddingItem,
+                verticalPadding: verticalPaddingItem,
+                content:
+                DisplayExpression(
+                    context: context,
+                    decoration: listItemDecoration,
+                    // This formula: r'.... \log_a 'frac{1}{a}...' do only work !!!
+                    expression: "b^y = a^{y\log_a b}",
+                    scale: leftColScale),
+                dialogContent:
+                DisplayExpression(
+                    context: context,
+                    decoration: listItemDecoration,
+                    expression: "a \\not\\equiv 1",
+                    scale: rightColScale),
+              ),
+              listItem(
+                horizontalPadding: horizontalPaddingItem,
+                verticalPadding: verticalPaddingItem,
+                content:
+                DisplayExpression(
+                    context: context,
+                    decoration: listItemDecoration,
+                    expression: "a = b^{log_b a} = a^{log_b a * log_a b}",
+                    scale: leftColScale),
+                dialogContent:
+                DisplayExpression(
+                    context: context,
+                    decoration: listItemDecoration,
+                    expression: "a \\not\\equiv 1, b \\not\\equiv 1",
+                    scale: rightColScale),
+              ),
+              listItem(
+                horizontalPadding: horizontalPaddingItem,
+                verticalPadding: verticalPaddingItem,
+                content:
+                DisplayExpression(
+                    context: context,
+                    decoration: listItemDecoration,
+                    expression: "1 = log_b a * log_a b",
+                    scale: leftColScale),
+                dialogContent:
+                DisplayExpression(
+                    context: context,
+                    decoration: listItemDecoration,
+                    expression: "a \\not\\equiv 1, b \\not\\equiv 1",
+                    scale: rightColScale),
+              ),
+              listItem(
+                horizontalPadding: horizontalPaddingItem,
+                verticalPadding: verticalPaddingItem,
+                content:
+                DisplayExpression(
+                    context: context,
+                    decoration: listItemDecoration,
+                    expression: "log_a uv = log_a u + log_a v",
+                    scale: leftColScale),
+                dialogContent:
+                DisplayExpression(
+                    context: context,
+                    decoration: listItemDecoration,
+                    expression: "u > 0, v > 0",
+                    scale: rightColScale),
+              ),
+              listItem(
+                horizontalPadding: horizontalPaddingItem,
+                verticalPadding: verticalPaddingItem,
+                content:
+                DisplayExpression(
+                    context: context,
+                    decoration: listItemDecoration,
+                    expression: "log_a uv = log_a u + log_a v",
+                    scale: leftColScale),
+                dialogContent:
+                DisplayExpression(
+                    context: context,
+                    decoration: listItemDecoration,
+                    expression: "v >0, v > 0",
+                    scale: rightColScale),
+              ),
+              listItem(
+                horizontalPadding: horizontalPaddingItem,
+                verticalPadding: verticalPaddingItem,
+                content:
+                DisplayExpression(
+                    context: context,
+                    decoration: listItemDecoration,
+                    expression: "log_a {u^z} = z\\log_a u",
+                    scale: leftColScale),
+                dialogContent:
+                DisplayExpression(
+                    context: context,
+                    decoration: listItemDecoration,
+                    expression: "u > 0",
+                    scale: rightColScale),
+              ),
+              listItem(
+                horizontalPadding: horizontalPaddingItem,
+                verticalPadding: verticalPaddingItem,
+                content:
+                DisplayExpression(
+                    context: context,
+                    decoration: listItemDecoration,
+                    // This formula: r'.... \log_a 'frac{1}{a}...' do only work !!!
+                    expression: r'log_a \frac{u}{v} = log_a u - log_a v',
+                    scale: leftColScale),
+                dialogContent:
+                DisplayExpression(
+                    context: context,
+                    decoration: listItemDecoration,
+                    expression: "a \\not\\equiv 1, v \\not\\equiv 0",
+                    scale: rightColScale),
+              ),
+              listItem(
+                horizontalPadding: horizontalPaddingItem,
+                verticalPadding: verticalPaddingItem,
+                content:
+                DisplayExpression(
+                    context: context,
+                    decoration: listItemDecoration,
+                    expression: "log_a u^{-1} = -log_a u",
+                    scale: leftColScale),
+                dialogContent:
+                DisplayExpression(
+                    context: context,
+                    decoration: listItemDecoration,
+                    expression: "a \\not\\equiv 1",
+                    scale: rightColScale),
+              ),
+            ],
+          )
         ),
       ),
     );
   }
 
-  TableRow tableRow({required double horizontalPadding, required double verticalPadding, required List<Widget> cells}) {
-    return TableRow(
-      children: [
-        for (var cell in cells)
-          TableCell(
-            verticalAlignment: TableCellVerticalAlignment.middle,
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: horizontalPadding, vertical: verticalPadding),
-              child: cell,
-            )
-          )
-      ]
+  Widget listItem(
+    {required double horizontalPadding,
+     required double verticalPadding,
+     required Widget content,
+     Widget? dialogContent}) {
+    // Use a GlobalKey to get the position of the item
+    final GlobalKey itemKey = GlobalKey();
+
+    return Container(
+      key: itemKey,
+      padding: EdgeInsets.symmetric(horizontal: horizontalPadding, vertical: verticalPadding),
+      child: GestureDetector(
+        onLongPress: () {
+          if (dialogContent != null) {
+            final RenderBox itemBox = itemKey.currentContext!.findRenderObject() as RenderBox;
+            final Offset itemPosition = itemBox.localToGlobal(Offset.zero);
+            final Size size = itemBox.size;
+
+            showMenu(
+              context: context,
+              menuPadding: EdgeInsets.zero,
+              elevation: 0,
+              color: Colors.transparent,
+              position: RelativeRect.fromLTRB(
+                itemPosition.dx + size.width,
+                itemPosition.dy + size.height / 2,
+                itemPosition.dx + size.width,
+                itemPosition.dy + size.height,
+              ),
+              items: [
+                PopupMenuItem(
+                  value: 0,
+                  padding: EdgeInsets.zero,
+                  child: dialogContent,
+                ),
+              ]
+            );
+          }
+        },
+        child: content,
+      )
     );
   }
 }
